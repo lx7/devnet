fb87b99492b996b7004044d24e0d942dd922c904
alex <alex@echocluster.net>
2020-12-06 19:39:13 +0100

    improve logging for testutil.Echo

--- internal/testutil/echohandler.go
+++ internal/testutil/echohandler.go
@@ -1,10 +1,10 @@
 package testutil
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -12,17 +12,20 @@ var upgrader = websocket.Upgrader{}
 func Echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("testutil echohandler upgrade error: ", err)
 	}
 	defer c.Close()
 
 	for {
 		mt, m, err := c.ReadMessage()
 		if err != nil {
-			log.Fatal(err)
+			if !websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+				log.Fatal("testutil echohandler unexpected close error: ", err)
+			}
+			return
 		}
 		if err := c.WriteMessage(mt, m); err != nil {
-			log.Fatal(err)
+			log.Fatal("testutil echohandler write error: ", err)
 		}
 	}
 }
