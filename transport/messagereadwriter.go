f2a08d479fc15c08af451316a14f22d06926c3b6
alex <alex@echocluster.net>
2020-12-06 14:25:36 +0100

    refactor to reduce complexity and make things more idiomatic

--- transport/messagereadwriter.go
+++
@@ -1,9 +0,0 @@
-package transport
-
-// MessageReadWriter is the common interface for signaling connections.
-// It defines ReadMessage, WriteMessage, Ready and Close.
-type MessageReadWriter interface {
-	ReadMessage() ([]byte, error)
-	WriteMessage([]byte) error
-	Close()
-}
